504686f8b7663f9a7390480e5292a722d810d642
jonbae <jonbae61596@gmail.com>
2021-01-28 19:09:21 -0500

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+const content = [
+  {
+    title: "woiejfwoeifjwoeijf", 
+    content: "woiejfwoeifjwoief"
+  },
+  {
+    title: "woiefjwoeifje",
+    content: "woiejfoweifjwoeifj"
+  }
+]
+
 function App() {
+  
+  const renderPostList = content.map( post => {
+    return (
+      <div> 
+        <div>
+          {post.title}
+        </div>
+        <div> 
+          {post.content}
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
         WE LIKE THE STOCK! WE LIKE THE STOCK! 
+
+
+        <header className="banner">
+          {/* hamburger  */}
+
+          {/* logo */}
+          <img /> 
+          {/* patreon   */}
+        </header>
+
+
+        <main>
+
+          {renderPostList}
+          
+        </main> 
+
+
     </div>
   );
 }
